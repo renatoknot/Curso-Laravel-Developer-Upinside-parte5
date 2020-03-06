31fd6f15b91726e92d9b0d1dd3b510327c787dc9
Renato Santos <renatoslip@hotmail.com>
2020-03-06 16:09:31 -0300

    relacionamento 1 pra n

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Address;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -58,6 +59,64 @@ class UserController extends Controller
             echo "Complemento: {$userAddress->complement}  CEP: {$userAddress->zipcode}<br>";
             echo "Cidade/Estado: {$userAddress->city}/{$userAddress->state}";
         }
+
+        // $addressTest = new Address([
+        //     'address' => 'Rua dos Bobos',
+        //     'number' => 180,
+        //     'complement'=> 'Antiga rua 35',
+        //     'zipcode' => '21725660',
+        //     'city' => 'Rio de Janeiro',
+        //     'state' => 'Rio de Janeiro'
+        // ]);
+
+        // $address = new Address();
+        // $address->address = 'Rua da Brisa';
+        // $address->number = 180;
+        // $address->complement = 'Antiga rua 35';
+        // $address->zipcode = '21725660';
+        // $address->city = 'Florianópolis';
+        // $address->state = 'Santa Catarina';
+
+        // $user->adressDelivery()->save($address);
+        // $user->adressDelivery()->saveMany([$addressTest, $address]);
+
+        // $user->adressDelivery()->create([
+        //     'address' => 'Rua dos Bobos',
+        //     'number' => 180,
+        //     'complement'=> 'Antiga rua 35',
+        //     'zipcode' => '21725660',
+        //     'city' => 'Rio de Janeiro',
+        //     'state' => 'Rio de Janeiro'
+        // ]);
+
+        // $user->adressDelivery()->createMany([[
+        //     'address' => 'Rua dos Bobos',
+        //     'number' => 180,
+        //     'complement'=> 'Antiga rua 35',
+        //     'zipcode' => '21725660',
+        //     'city' => 'Rio de Janeiro',
+        //     'state' => 'Rio de Janeiro'
+        // ], [
+        //     'address' => 'Rua Tessália',
+        //     'number' => 17,
+        //     'complement'=> '',
+        //     'zipcode' => '21210120',
+        //     'city' => 'Rio de Janeiro',
+        //     'state' => 'Rio de Janeiro'
+        // ]]);
+
+        // $users = User::with('adressDelivery')->get();
+        // dump($users);
+        $posts = $user->posts()->orderBy('id', 'DESC')->take(2)->get();
+        if($posts){
+            echo "<h1>Artigos</h1><br>";
+            foreach($posts as $post) {
+                echo "#{$post->id}  Titulo: {$post->title}<br>";
+                echo "Subtitulo: {$post->subtitle} <br>";
+                echo "Conteúdo: {$post->description}<br><hr><br><br>";
+            }
+        }
+        // dump($posts);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::resource('users', 'UserController');
 Route::resource('addresses', 'AddressController');
+Route::resource('posts', 'PostController');
 
 Route::get('/', function () {
     return view('welcome');
